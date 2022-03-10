e93496d791ce744784378568c8065fb854ece8b0
Adithyakeshava K <adithyakak0@gmail.com>
2022-03-11 03:06:04 +0530

    Spring app updated

--- SpringBootPractiseApp/src/main/java/com/webapp/SpringBootMain.java
+++ SpringBootPractiseApp/src/main/java/com/webapp/SpringBootMain.java
@@ -1,41 +1,13 @@
 package com.webapp;
 
-import com.typesafe.config.Config;
-import com.typesafe.config.ConfigFactory;
-import com.webapp.entity.Product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Properties;
-
-import static com.webapp.constants.Constants.*;
 
 @SpringBootApplication
 public class SpringBootMain {
 
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(SpringBootMain.class);
-        Properties properties = new Properties();
-
-        try {
-            Properties props= new Properties();
-            props.load(new BufferedReader(new FileReader(new File(""))));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        Config config = ConfigFactory.parseFile(new File(args[0]));
-        properties.put(SERVER_PORT, config.getString(SERVER_PORT));
-        properties.put(DATASOURCE_URL, config.getString(DATASOURCE_URL));
-        properties.put(DATASOURCE_USERNAME, config.getString(DATASOURCE_USERNAME));
-        properties.put(DATASOURCE_PASSWORD, config.getString(DATASOURCE_PASSWORD));
-        properties.put(DRIVER_CLASS_NAME, config.getString(DRIVER_CLASS_NAME));
-        app.setDefaultProperties(properties);
-        app.run(args);
+        new SpringApplication(SpringBootMain.class).run();
     }
 
 }
--- SpringBootPractiseApp/src/main/java/com/webapp/controller/RestApiController.java
+++ SpringBootPractiseApp/src/main/java/com/webapp/controller/RestApiController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/spring")
 public class RestApiController {
     @Autowired
     ProductService service;
